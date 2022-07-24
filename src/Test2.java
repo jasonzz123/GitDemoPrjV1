4b679c29d28cb4628020b6ff10690c43562449d7
ASUS <ASUS@192.168.0.82>
2022-07-25 02:14:59 +0800

    do more check

--- src/Test2.java
+++ src/Test2.java
@@ -3,6 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("more check");
 		System.out.println("Test2 eat something!");
 	}
 
